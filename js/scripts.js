c5e9d7304acc7025af7b82c0e58d3809c07a4f8b
JPZCode <jpzcode02@gmail.com>
2019-04-22 22:22:52 -0500

    Clonar contenido

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $(function() { //Función que carga JQuery
+  'use strict';
 
+  //$('main article:first').remove(); // Elimina del DOM.
+  //$('main article:first').hide(); // Oculta
 
+    $('main article:last').clone();
+    //$('main').append(copia);  // Lo coloca antes de la copia
+    //$('main').prepend(copia); // Lo coloca al inicio
+
+    var copia = $('main article:last').clone();
+    $(copia).appendTo('main');
 
 });
