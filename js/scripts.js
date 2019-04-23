d39ab96e5e34db1e5f4813f489bda0e312370df6
JPZCode <jpzcode02@gmail.com>
2019-04-22 22:33:58 -0500

    MostrarOcultar

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,36 @@ $(function() { //Función que carga JQuery
 
   //$('main article:first').remove(); // Elimina del DOM.
   //$('main article:first').hide(); // Oculta
-
+/*
     $('main article:last').clone();
     //$('main').append(copia);  // Lo coloca antes de la copia
     //$('main').prepend(copia); // Lo coloca al inicio
 
     var copia = $('main article:last').clone();
     $(copia).appendTo('main');
+*/
+
+/* Mostrar u Ocultar contenido */
+
+/*$('div.logo img').on('click', function(){ //Eventos
+  console.log("Has echo click.");
+});*/
+
+/* $('div.logo img').on('click', function(){
+  $(this).remove();
+}); */
+
+/* $('div.logo img').on('mouseenter', function () {
+  console.log("Sobre el logo");
+});
+$('div.logo img').on('mouseleave', function () {
+  console.log("Fuera del logo")
+}); */
+
+
+$('#menu').on('click', function(){
+  $('#navegacion').show();
+});
+
 
 });
